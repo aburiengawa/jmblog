13ce798d71c9c072f468640e062d06c2de0e11da
JM Taylor <jmotaylor2@gmail.com>
2018-01-16 19:31:20 +0900

    Finish TinyMCE customization

--- resources/views/includes/tinyeditor.blade.php
+++ resources/views/includes/tinyeditor.blade.php
@@ -2,8 +2,62 @@
 {{-- <textarea name="content" class="form-control my-editor">{!! old('content', $content) !!}</textarea> --}}
 <script>
   var editor_config = {
+    
+style_formats: [
+        {title: 'Headers', items: [
+            {title: 'Header 1', format: 'h1'},
+            {title: 'Header 2', format: 'h2'},
+            {title: 'Header 3', format: 'h3'},
+            {title: 'Header 4', format: 'h4'},
+            {title: 'Header 5', format: 'h5'},
+            {title: 'Header 6', format: 'h6'}
+        ]},
+        {title: 'Inline', items: [
+            {title: 'Bold', icon: 'bold', format: 'bold'},
+            {title: 'Italic', icon: 'italic', format: 'italic'},
+            {title: 'Underline', icon: 'underline', format: 'underline'},
+            {title: 'Strikethrough', icon: 'strikethrough', format: 'strikethrough'},
+            {title: 'Superscript', icon: 'superscript', format: 'superscript'},
+            {title: 'Subscript', icon: 'subscript', format: 'subscript'}
+        ]},
+        {title: 'Blocks', items: [
+            {title: 'Paragraph', format: 'p'},
+            {title: 'Blockquote', format: 'blockquote'}
+        ]},
+        {title: 'Alignment', items: [
+            {title: 'Left', icon: 'alignleft', format: 'alignleft'},
+            {title: 'Center', icon: 'aligncenter', format: 'aligncenter'},
+            {title: 'Right', icon: 'alignright', format: 'alignright'},
+            {title: 'Justify', icon: 'alignjustify', format: 'alignjustify'}
+        ]},
+        {title: 'Caption', inline: 'span', classes: 'caption text-muted'},
+        {title: 'Section Heading', block: 'h2', classes: 'section-heading'}
+    ],
+
+    formats: {
+      alignleft: {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'left'},
+      aligncenter: {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'center'},
+      alignright: {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'right'},
+      alignjustify: {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'full'},
+      // h2: {block: 'h2', classes: 'section-heading'},
+      blockquote: {block : 'blockquote', classes : 'blockquote'},
+      bold: {inline : 'span', 'classes' : 'bold'},
+      italic: {inline : 'span', 'classes' : 'italic'},
+      underline: {inline : 'span', 'classes' : 'underline', exact : true},
+      strikethrough: {inline : 'del'},
+      forecolor: {inline : 'span', classes : 'forecolor', styles : {color : '%value'}},
+      hilitecolor: {inline : 'span', classes : 'hilitecolor', styles : {backgroundColor : '%value'}},
+      custom_format: {block : 'h1', attributes : {title : 'Header'}, styles : {color : 'red'}}
+    },
+
+    image_class_list: [
+      {title: 'None', value: ''},
+      {title: 'img-fluid', value: 'img-fluid'},
+    ],
+    
     path_absolute : "/",
     selector: "textarea",
+
     plugins: [
       "advlist autolink lists link image charmap print preview hr anchor pagebreak",
       "searchreplace wordcount visualblocks visualchars code fullscreen",
