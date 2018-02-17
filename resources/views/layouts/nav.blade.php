11c05613812616914fc53c0d48aa7b78a3397dec
JM Taylor <jmotaylor2@gmail.com>
2018-02-17 23:53:52 +0900

    Fix navbar layout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,39 +1,39 @@
 <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand d-flex mr-auto" href="index.html">Start Bootstrap</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="fa fa-bars"></i>
+            Menu
+            <i class="fa fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('/') }}">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-            @if (Route::has('login'))
-                {{-- <div class="top-right links"> --}}
-                    @auth
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/admin') }}">Admin</a>
-                    </li>                        
-                    @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>  
-                    <li class="nav-item">                        
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
-                    </li>
-                    @endauth
-                {{-- </div> --}}
-            @endif            
-          </ul>
-        </div>
-      </div>
-    </nav>
+        <div class="navbar-collapse collapse w-100" id="navbarResponsive">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>             
+            </ul>
+        @if (Route::has('login'))
+            <ul class="navbar-nav float-right">
+            @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/admin') }}">Admin</a>
+                </li>                        
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>  
+                <li class="nav-item">                        
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+            @endauth
+            </ul>
+        @endif
+        </div> {{-- .navbar-collapse --}}
+    </div> {{-- .container --}}
+</nav>
