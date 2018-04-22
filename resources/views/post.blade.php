5ec99f476edffb12da15af197291ac5309b3b189
JM Taylor <jmotaylor2@gmail.com>
2018-04-22 17:11:36 +1000

    Fix margin of reply

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -27,7 +27,6 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
-
             @if($post->tags->isNotEmpty())
                 Tags: 
                 @foreach($post->tags as $tag)
@@ -37,41 +36,13 @@
                 @endforeach
               {{-- </p> --}}
             @endif
-
             {!! $post->body !!}
-
-{{--             <h2 class="section-heading">The Final Frontier</h2>
-
-            <p>There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning.</p>
-
-            <p>There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning.</p>
-
-            <blockquote class="blockquote">The dreams of yesterday are the hopes of today and the reality of tomorrow. Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next ten.</blockquote>
-
-            <p>Spaceflights cannot be stopped. This is not the work of any one man or even a group of men. It is a historical process which mankind is carrying out in accordance with the natural laws of human development.</p>
-
-            <h2 class="section-heading">Reaching for the Stars</h2>
-
-            <p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
-
-            <a href="#">
-              <img class="img-fluid" src="/img/post-sample-image.jpg" alt="">
-            </a>
-            <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span>
-
-            <p>Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
-
-            <p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore, and this is exploration at its greatest.</p>
-
-            <p>Placeholder text by
-              <a href="http://spaceipsum.com/">Space Ipsum</a>. Photographs by
-              <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>.</p> --}}
-                <hr>
-                <!-- Blog Comments -->
-                <!-- Comments Form -->
-                @auth
-                  <div class="well">
-                      <h4>Leave a Comment:</h4>
+            <hr>
+              <!-- Blog Comments -->
+              <!-- Comments Form -->
+              @auth
+                <div class="well">
+                  <h4>Leave a Comment:</h4>
                   {!! Form::open(['method'=>'POST', 'action'=>'AdminCommentsController@store', 'files'=>true]) !!}
                   <input type="hidden" name="post_id" value="{{$post->id}}">
                   <div class="form-group">
@@ -82,73 +53,61 @@
                     {!! Form::submit('Comment', ['class'=>'btn btn-primary']) !!}
                   </div>
                   {!! Form::close() !!}
-                  </div> <!-- .well -->
-                @endauth
-{{--                     <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div> --}}
+                </div> <!-- .well -->
+              @endauth
                 <hr>
                 <!-- Posted Comments -->
                 <!-- Comment -->
                 @if($post->comments->isNotEmpty())
                   @foreach($post->comments as $comment)
                   <div class="media">
-{{--                     <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a> --}}
                     <div class="media-body">
-                        <h4 class="media-heading">{{$comment->user->name}}
-                            <small>{{$comment->created_at->diffForHumans()}}</small>
-                        </h4>
-                        {{$comment->body}}
-                        {!! Form::open(['method'=>'POST', 'action'=>'AdminRepliesController@store', 'files'=>true]) !!}
-                        <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                        <div class="form-group">
-                          {!! Form::label('body', 'Content:') !!}
-                          {!! Form::textarea('body', null, ['class'=>'form-control', 'rows' => 2]) !!}
-                        </div>  
-                        <div class="form-group">
-                          {!! Form::submit('Reply', ['class'=>'btn btn-primary']) !!}
-                        </div>
-                        {!! Form::close() !!}
+                      <h4 class="media-heading">{{$comment->user->name}}
+                        <small>{{$comment->created_at->diffForHumans()}}</small>
+                      </h4>
+                      {{$comment->body}}
+                      {!! Form::open(['method'=>'POST', 'action'=>'AdminRepliesController@store', 'files'=>true]) !!}
+                      <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                      <div class="form-group">
+                        {!! Form::label('body', 'Content:') !!}
+                        {!! Form::textarea('body', null, ['class'=>'form-control', 'rows' => 2]) !!}
+                      </div>  
+                      <div class="form-group">
+                        {!! Form::submit('Reply', ['class'=>'btn btn-primary']) !!}
+                      </div>
+                      {!! Form::close() !!}
+                        {{-- Reply --}}
                         @if($comment->replies->isNotEmpty())
                           @foreach($comment->replies as $reply)
-{{--                             <div class="media">
-                              <a class="pull-left" href="#">
-                                  <img class="media-object" src="http://placehold.it/64x64" alt="">
-                              </a> --}}
-                              <div class="media-body">
-                                  <h4 class="media-heading">{{$reply->user->name}}
-                                      <small>{{$reply->created_at->diffForHumans()}}</small>
-                                  </h4>
-                                  {{$reply->body}}
-                                  {!! Form::open(['method'=>'POST', 'action'=>'AdminRepliesController@store', 'files'=>true]) !!}
-                                  <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                                  <div class="form-group">
-                                    {!! Form::label('body', 'Content:') !!}
-                                    {!! Form::textarea('body', null, ['class'=>'form-control', 'rows' => 2]) !!}
-                                  </div>  
-                                  <div class="form-group">
-                                    {!! Form::submit('Reply', ['class'=>'btn btn-primary']) !!}
-                                  </div>
-                                  {!! Form::close() !!}
+                          <div class="media ml-5">
+                            <div class="media-body">
+                              <h4 class="media-heading">{{$reply->user->name}}
+                                <small>{{$reply->created_at->diffForHumans()}}</small>
+                              </h4>
+                              {{$reply->body}}
+                              {!! Form::open(['method'=>'POST', 'action'=>'AdminRepliesController@store', 'files'=>true]) !!}
+                              <input type="hidden" name="comment_id" value="{{$comment->id}}">
+                              <div class="form-group">
+                                {!! Form::label('body', 'Content:') !!}
+                                {!! Form::textarea('body', null, ['class'=>'form-control', 'rows' => 2]) !!}
+                              </div>  
+                              <div class="form-group">
+                                {!! Form::submit('Reply', ['class'=>'btn btn-primary']) !!}
                               </div>
-                          </div>
+                              {!! Form::close() !!}
+                            </div> {{-- Reply .media-body --}}
+                          </div> {{-- Reply .media --}}
                           @endforeach
                         @endif
-                    </div>
-                  </div>
+                    </div> {{-- Comment .media-body --}}
+                  </div> {{-- Comment .media --}}
                   @endforeach
                 @endif
                 <!-- Comment -->
                 <div class="media">
-                    <a class="pull-left" href="#">
+{{--                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
+                    </a> --}}
                     <div class="media-body">
                         <h4 class="media-heading">Start Bootstrap
                             <small>August 25, 2014 at 9:30 PM</small>
@@ -156,9 +115,9 @@
                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                         <!-- Nested Comment -->
                         <div class="media">
-                            <a class="pull-left" href="#">
+{{--                             <a class="pull-left" href="#">
                                 <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
+                            </a> --}}
                             <div class="media-body">
                                 <h4 class="media-heading">Nested Start Bootstrap
                                     <small>August 25, 2014 at 9:30 PM</small>
@@ -177,3 +136,6 @@
     </article>
 
     <hr>
+    <script>
+      
+    </script>
