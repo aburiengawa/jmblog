e8d71d9c930465f90ce799e4755e3bd974394cd9
JM Taylor <jmotaylor2@gmail.com>
2018-05-15 02:20:32 +0900

    Include local jquery file

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,4 +19,5 @@
         <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> --}}
+    <script src="{{asset('js/jquery-3.3.1.js')}}"></script>
